08487085460c23e75b0115e03a84c4b63ce822bc
AlbertoIJS <aijs.cdmx@gmail.com>
2023-06-22 19:37:00 -0600

    TODO: Fix initializeVariants function

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
 
+
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [variants, setVariants] = useState(initializeVariants(VARIANT_OPTIONS));
+
+  function initializeVariants(variants) {
+    const obj = {}
+    return variants.map(variant => obj[variant] = "")
+  }
+
+  console.log(variants)
   return (
     <div className={styles.wrapper}>
       <header>
-        <img alt="Cute toast mascot" src="/toast.png" />
+        <img alt="Cute toast mascot" src="/toast.png"/>
         <h1>Toast Playground</h1>
       </header>
 
@@ -19,12 +28,12 @@ function ToastPlayground() {
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{alignSelf: 'baseline'}}
           >
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" className={styles.messageInput}/>
           </div>
         </div>
 
@@ -33,22 +42,26 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
+            {
+              VARIANT_OPTIONS.map((variant) => (
+                <label htmlFor="variant-notice" key={variant}>
+                  <input
+                    id="variant-notice"
+                    type="radio"
+                    name="variant"
+                    // value={}
+                    // onClick={() =>}
+                  />
+                  notice
+                </label>
+              ))
+            }
 
-            {/* TODO Other Variant radio buttons here */}
           </div>
         </div>
 
         <div className={styles.row}>
-          <div className={styles.label} />
+          <div className={styles.label}/>
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
